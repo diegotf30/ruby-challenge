f3e12fec8c7d3b651f02479688e0ee014211197c
Diego Trevino <diegotf30@gmail.com>
2018-01-18 10:32:48 -0600

    Finished Challenge
    Script outputs to console number of lines each character (except ALL) says

--- analyzing-shakespeare/macbeth_analyzer.rb
+++ analyzing-shakespeare/macbeth_analyzer.rb
@@ -1,2 +1,19 @@
+require 'open-uri'
 require 'nokogiri'
 
+doc = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
+
+speakers = Hash.new(0) #Speaker => no. of lines, starts at 0
+speeches = doc.xpath('//SPEECH')
+
+speeches.each do |speech|
+  curr_speaker = speech.at_xpath('SPEAKER').content #Gets name of the current speaker
+  unless curr_speaker == 'ALL'
+    lines = speech.xpath('LINE') # Gets all the lines curr_speaker says
+    speakers[curr_speaker] += lines.length
+  end
+end
+
+speakers.each do |key, value|
+    puts value.to_s + ' ' + key.capitalize
+end
